52fd8951f4cf550c58e0f582412a7da5ceaa6c5e
JustOxxy <oksundel@mail.ru>
2017-08-17 18:32:07 +0300

    Update method bye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def hello
+    render text: "hello, Meeshka"
+  end
 
+  def bye
+    render text: "BYE"
+  end
 end
